9681296f4fe0a4f13ac1ea52ed54a6cad4ae0738
tosuiii <guizinhozy@gmail.com>
2026-08-27 20:20:16 -0300

    Delete Agent directory

--- Agent/BuildConfig.cs
+++
@@ -1,7 +0,0 @@
-namespace EmpresaMonitor.Agent;
-
-internal static class BuildConfig
-{
-    public const string ServerUrl = "https://SEU-SITE.netlify.app";
-    public const string AgentKey = "SUA-AGENT-KEY";
-}
