6eaa369a90a7125f757f709e6dbf6abe6cfbb732
tosuiii <guizinhozy@gmail.com>
2026-08-30 19:35:00 -0300

    Delete Agent directory

--- Agent/BuildConfig.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace EmpresaMonitor.Agent;
-internal static class BuildConfig
-{
-    public const string RealtimeUrl = "wss://kltochique-production.up.railway.app";
-    public const string AgentKey = "tochique123";
-}
