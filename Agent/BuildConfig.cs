6cda5103740ea29bef7b387aa51cb177af7acac9
tosuiii <guizinhozy@gmail.com>
2026-08-30 05:16:28 -0300

    Delete Agent directory

--- Agent/BuildConfig.cs
+++
@@ -1,7 +0,0 @@
-namespace EmpresaMonitor.Agent;
-internal static class BuildConfig
-{
-    // Este arquivo é regravado automaticamente pelo BUILD_AGENT.ps1.
-    public const string RealtimeUrl = "wss://SEU-SERVIDOR";
-    public const string AgentKey = "CONFIGURE_PELO_BUILD_AGENT";
-}
