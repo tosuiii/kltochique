05be117f090d8160e466188fb26c177ef7015758
tosuiii <guizinhozy@gmail.com>
2026-08-29 21:46:07 -0300

    Delete Agent directory

--- Agent/BuildConfig.cs
+++
@@ -1,7 +0,0 @@
-namespace EmpresaMonitor.Agent;
-
-internal static class BuildConfig
-{
-    public const string RealtimeUrl = "wss://SEU-REALTIME.onrender.com";
-    public const string AgentKey = "SUA-AGENT-KEY";
-}
